617833ea2f95fcee2b131cb1459c3c53b5b70b47
Gr34sy <slupek3@poczta.fm>
2023-07-16 18:12:12 +0200

    added scss files and copied navbar component from prelux porfolio

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Logo() {
+  return (
+    <div className="logo">
+      <a href="#">
+        <p className="logotype">ToDo</p>
+      </a>
+    </div>
+  );
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import { Logo } from "../Logo/Logo";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+
+export function Navbar(){
+    const [scroll, setScroll] = useState(0);
+    const [showMobileList, setShowMobileList] = useState(false);
+
+    useEffect(() =>{
+        window.addEventListener('scroll', () => {
+            setScroll(window.scrollY);
+        });
+
+        return () => setScroll(0);
+    },[])
+
+    return(
+        <>
+        <nav className={scroll>50 ? 'navbar sticky' : 'navbar'} onScroll={(e) => e.currentTarget.class="scroll"}>
+            <div className="content">
+                <Logo/>
+
+                <ul className="nav__list">
+                    <li><a className="custom-link" href="#hero">Home</a></li>
+                    <li><a className="custom-link" href="#about">About Me</a></li>
+                    <li><a className="custom-link" href="#gallery">Gallery</a></li>
+                    <li><a className="custom-link" href="#footer">Contact</a></li>
+                </ul>
+
+                { !showMobileList && <div className="hamburger-icon" onClick={() => setShowMobileList(true)}>
+                    <FontAwesomeIcon icon={faBars}/>
+                </div>}
+
+                { showMobileList && <div className="x-icon" onClick={() => setShowMobileList(false)}>
+                    <FontAwesomeIcon icon={faXmark}/>
+                </div>}
+            </div>
+        </nav>
+        
+        { showMobileList && 
+        <ul className="nav__list--mobile">
+            <li><a className="custom-link" href="#hero">Home</a></li>
+            <li><a className="custom-link" href="#about">About Me</a></li>
+            <li><a className="custom-link" href="#gallery">Gallery</a></li>
+            <li><a className="custom-link" href="#footer">Contact</a></li>
+        </ul>}
+        </>
+    )
+}
