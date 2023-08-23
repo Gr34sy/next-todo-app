66025fded680407062fa4855608da1407646aa5f
Gr34sy <slupek3@poczta.fm>
2023-08-23 14:47:06 +0200

    started footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,19 @@
-export function Footer(){
-    return (
-        <footer className="footer">
-            footer
-        </footer>
-    )
+import { Logo } from "../Logo/Logo";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__copytight">
+        <h2 className="copyright__header">
+          <Logo />
+          Copyright by Peter Slupski 2023 &copy;
+        </h2>
+      </div>
+
+      <div className="footer__links">
+        <p>My LinkedIn</p>
+        <p>Check my profile page</p>
+      </div>
+    </footer>
+  );
 }
