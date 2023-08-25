63c3c00407a82ebe81c7bac313fc3806e6121959
Gr34sy <slupek3@poczta.fm>
2023-08-25 11:33:26 +0200

    finished responsive footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,8 @@
 import { Logo } from "../Logo/Logo";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faUser} from '@fortawesome/free-solid-svg-icons'
 
 export function Footer() {
   return (
@@ -12,20 +15,23 @@ export function Footer() {
       </div>
 
       <div className="footer__links">
-        <Link href="page: https://heroic-quokka-3e69bf.netlify.app">
+        <Link href="https://heroic-quokka-3e69bf.netlify.app">
           <p className="footer__link">
+            <FontAwesomeIcon icon={faUser} className="footer__icon"/>
             Check my profile page
           </p>
         </Link>
 
         <Link href="https://github.com/Gr34sy">
           <p className="footer__link">
+            <FontAwesomeIcon icon={faGithub} className="footer__icon"/>
             Check my Github
           </p>
         </Link>
 
         <Link href="https://linkedin.com/in/piotr-słupski-greasy17">
           <p className="footer__link">
+            <FontAwesomeIcon icon={faLinkedin} className="footer__icon"/>
             Check my LinkedIn
           </p>
         </Link>
