37ebfba149bde6f782355c79260ff97df4936598
Gr34sy <slupek3@poczta.fm>
2023-09-03 23:33:28 +0200

    finished remaining form themes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='midnight-green'>
+      <body className='default'>
         <Main />
         <NextScript />
       </body>
