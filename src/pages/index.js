09834f8e4bf68c45ec1f4c7d781da4aa7e63edb7
Gr34sy <slupek3@poczta.fm>
2023-09-04 15:00:08 +0200

    started app-info section styling in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
-import { FormCreateTaskList } from "@/components/Forms/FormCreateTaskList";
-
 export default function HomePage() {
   return (
     <main>
-      <div style={{ height: "200vh" }}>
-        <FormCreateTaskList />
-      </div>
+      <section className="app-info section section--colored">
+        <div className="app-info__wrapper">
+          <div className="app-info__pictures">
+            <div className="pic1"></div>
+
+            <div className="pic2"></div>
+          </div>
+
+          <div className="app-info__content">
+            <h1 className="app-info__header">
+                About the app
+            </h1>
+
+            <p className="app-info__description">
+              In ToDo App (logotype) you can not only create a simple todo list, but you can also create a much advanced tasklist. 
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
