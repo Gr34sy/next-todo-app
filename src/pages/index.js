73fe7e7c2ce5107a364c7e5d2c3903ccfa5fa0cc
Gr34sy <slupek3@poczta.fm>
2023-09-08 17:24:22 +0200

    added responsive pictures to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,38 @@
+import Image from "next/image";
+
 export default function HomePage() {
   return (
     <main>
       <section className="app-info section section--colored">
         <div className="app-info__wrapper">
           <div className="app-info__pictures">
-            <div className="pic1"></div>
+            <div className="pic1">
+              <Image
+                className="app-info__image"
+                src="/images/CoffeeNotes.jpg"
+                fill={true}
+                alt="Coffe Notes"
+              />
+            </div>
 
-            <div className="pic2"></div>
+            <div className="pic2">
+              <Image
+                className="app-info__image"
+                src="/images/Notes.jpg"
+                fill={true}
+                alt="Notes"
+              />
+            </div>
           </div>
-
-        
 
           <div className="app-info__content">
-            <h1 className="app-info__header">
-                About the app
-            </h1>
+            <h1 className="app-info__header">About the app</h1>
 
             <p className="app-info__description">
-              In ToDo App (logotype) you can not only create a simple todo list, but you can also create a much advanced tasklist. 
+              In ToDo App (logotype) you can not only create a simple todo list,
+              but you can also create a much advanced tasklist.
             </p>
           </div>
-
         </div>
       </section>
     </main>
