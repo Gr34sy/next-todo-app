e2e893e4172afbbe2181a58d3c9e57ec97d4d2b4
Gr34sy <slupek3@poczta.fm>
2023-09-10 00:26:54 +0200

    started adding app description to app-info in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Logo } from "@/components/Logo/Logo";
+import { FormCreateTaskList } from "@/components/Forms/FormCreateTaskList";
 
 export default function HomePage() {
   return (
@@ -35,14 +36,25 @@ export default function HomePage() {
 
             <div className="app-info__description">
               <p className="app-info__description_p">
-                In this app you can not only create a simple todo list, but you can also
-                create a much advanced lists which are called 
-                <span className="text-highlight"> tasklists</span>.
+                When using this app you can not only create a simple todo list, but you
+                can also create a much advanced lists which are called
+                <span className="text-highlight"> tasklists</span>. Tasklist
+                enables to add deadline, tags, operations and description. There is also possibility to mark the checkmark in order to display the list in pinned lists section.
+                Operations are kind of subtasks, necessary to complete the main task.
+                Each operation consists of estimated time needed to complete it and description.
+              </p>
+
+              <p className="app-info__description_p">
+                There is also a bunch of theme options so you can choose your
+                facorite color motive. Among others, application offers
+                celestial blue theme, orange theme,
+                <span className="text-highlight"> nightmode</span> and more.
               </p>
             </div>
           </div>
         </div>
       </section>
+      <FormCreateTaskList />
     </main>
   );
 }
