b49f556fbd38db5913e9a94baee863ac214f562b
Gr34sy <slupek3@poczta.fm>
2023-09-12 03:29:31 +0200

    added theme changing option

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="tiffany-blue">
+      <body className="default">
         <Main />
         <NextScript />
       </body>
--- src/pages/options/index.js
+++ src/pages/options/index.js
@@ -1,7 +1,28 @@
 function OptionsPage() {
+  function colorThemeHandler(e) {
+    e.preventDefault();
+    document.body.className = "";
+    document.body.classList.add(e.target.dataset.value);
+  }
+
   return (
-    <section>
+    <section className="section options">
       <h1>Options Page</h1>
+      <div>
+        <button className="custom-link" data-value="default" onClick={colorThemeHandler}>Default</button>
+
+        <button className="custom-link" data-value="celestial-blue" onClick={colorThemeHandler}>Celestial Blue</button>
+
+        <button className="custom-link" data-value="midnight-green" onClick={colorThemeHandler}>Midnight Green</button>
+
+        <button className="custom-link" data-value="nightmode" onClick={colorThemeHandler}>Nightmode</button>
+
+        <button className="custom-link" data-value="pinky" onClick={colorThemeHandler}>Pinky</button>
+
+        <button className="custom-link" data-value="orange" onClick={colorThemeHandler}>Orange</button>
+
+        <button className="custom-link" data-value="tiffany-blue" onClick={colorThemeHandler}>Tiffany Blue</button>
+      </div>
     </section>
   );
 }
