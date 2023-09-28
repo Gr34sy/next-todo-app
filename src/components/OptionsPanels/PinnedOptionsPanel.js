0e7acc8fa097a1f591607f6106da537a29ba6898
Gr34sy <slupek3@poczta.fm>
2023-09-28 20:31:22 +0200

    added custom-checkbox elements

--- src/components/OptionsPanels/PinnedOptionsPanel.js
+++ src/components/OptionsPanels/PinnedOptionsPanel.js
@@ -1,12 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 export function PinnedOptionsPanel() {
   const [chosenColorTheme, setChosenColorTheme] = useState("");
   const [pinnedLists, setPinnedLists] = useState(null);
-  const [pinedTasklist, setPinnedTasklist] = useState(null);
+  const [pinnedTasklist, setPinnedTasklist] = useState(null);
 
   function pinnedListChangeHandler(e) {
-    setPinnedLists(e.target.value);
+    const clickedCheckbox = document.querySelector(`.pinned-panel__lists-radios [for=${e.target.value}] .checkmark__div`);
+    console.log(clickedCheckbox);
+
+    if(pinnedLists === e.target.value){
+        setPinnedLists(null);
+        clickedCheckbox.classList.remove('checked');
+    }else{
+        const checkboxes = document.querySelectorAll('.pinned-panel__lists-radios .checkmark__div');
+        checkboxes.forEach((checkbox) => checkbox.classList.remove('checked'));
+
+        setPinnedLists(e.target.value);
+        clickedCheckbox.classList.add('checked');
+    }
+  }
+
+  function pinnedTasklistChangeHandler(e) {
+    setPinnedTasklist(e.target.value);
   }
 
   return (
@@ -14,87 +32,80 @@ export function PinnedOptionsPanel() {
       <div className="pinned-panel__radios">
         <div className="pinned-panel__lists-radios">
           <h2 className="menu-content__h2">Set your pinned simple lists</h2>
+
           <label htmlFor="pinned-list-1" className="menu-content__radio">
             <div className="menu-content__color-block color-block--default"></div>
+            <div className='checkmark__div'><FontAwesomeIcon className="checkmark__icon" icon={faCheck}/></div>
             <input
-              type="radio"
+              className="custom-checkbox"
+              type="checkbox"
               name="pinned-lists"
               id="pinned-list-1"
               value="pinned-list-1"
               onChange={pinnedListChangeHandler}
               checked={pinnedLists === "pinned-list-1"}
             />
-            <p className="menu-content__p">List 1</p>
+            <p className="menu-content__p">Tasklist 1</p>
           </label>
 
           <label htmlFor="pinned-list-2" className="menu-content__radio">
-            <div className="menu-content__color-block color-block--pinky"></div>
+            <div className="menu-content__color-block color-block--default"></div>
+            <div className='checkmark__div'><FontAwesomeIcon className="checkmark__icon" icon={faCheck}/></div>
             <input
-              type="radio"
+              className="custom-checkbox"
+              type="checkbox"
               name="pinned-lists"
               id="pinned-list-2"
               value="pinned-list-2"
               onChange={pinnedListChangeHandler}
               checked={pinnedLists === "pinned-list-2"}
             />
-            <p className="menu-content__p">List 2</p>
-          </label>
-
-          <label htmlFor="pinned-list-3" className="menu-content__radio">
-            <div className="menu-content__color-block color-block--pinky"></div>
-            <input
-              type="radio"
-              name="pinned-lists"
-              id="pinned-list-3"
-              value="pinned-list-3"
-              onChange={pinnedListChangeHandler}
-              checked={pinnedLists === "pinned-list-3"}
-            />
-            <p className="menu-content__p">List 3</p>
-          </label>
-
-          <label htmlFor="pinned-list-4" className="menu-content__radio">
-            <div className="menu-content__color-block color-block--pinky"></div>
-            <input
-              type="radio"
-              name="pinned-lists"
-              id="pinned-list-4"
-              value="pinned-list-4"
-              onChange={pinnedListChangeHandler}
-              checked={pinnedLists === "pinned-list-4"}
-            />
-            <p className="menu-content__p">List 4</p>
-          </label>
-
-          <label htmlFor="pinned-list-5" className="menu-content__radio">
-            <div className="menu-content__color-block color-block--pinky"></div>
-            <input
-              type="radio"
-              name="pinned-lists"
-              id="pinned-list-5"
-              value="pinned-list-5"
-              onChange={pinnedListChangeHandler}
-              checked={pinnedLists === "pinned-list-5"}
-            />
-            <p className="menu-content__p">List 5</p>
-          </label>
-
-          <label htmlFor="pinned-list-6" className="menu-content__radio">
-            <div className="menu-content__color-block color-block--orange"></div>
-            <input
-              type="radio"
-              name="pinned-lists"
-              id="pinned-list-6"
-              value="pinned-list-6"
-              onChange={pinnedListChangeHandler}
-              checked={pinnedLists === "pinned-list-6"}
-            />
-            <p className="menu-content__p">List 6</p>
+            <p className="menu-content__p">Tasklist 2</p>
           </label>
         </div>
 
         <div className="pinned-panel__tasklists-radios">
           <h2 className="menu-content__h2">Set your pinned tasklist</h2>
+
+          <label htmlFor="pinned-tasklist-1" className="menu-content__radio">
+            <div className="menu-content__color-block color-block--default"></div>
+            <input
+              type="radio"
+              name="pinned-lists"
+              id="pinned-tasklist-1"
+              value="pinned-tasklist-1"
+              onChange={pinnedTasklistChangeHandler}
+              checked={pinnedTasklist === "pinned-tasklist-1"}
+            />
+            <p className="menu-content__p">Tasklist 1</p>
+          </label>
+
+          <label htmlFor="pinned-tasklist-2" className="menu-content__radio">
+            <div className="menu-content__color-block color-block--default"></div>
+            <input
+              type="radio"
+              name="pinned-lists"
+              id="pinned-tasklist-2"
+              value="pinned-tasklist-2"
+              onChange={pinnedTasklistChangeHandler}
+              checked={pinnedTasklist === "pinned-tasklist-2"}
+            />
+            <p className="menu-content__p">Tasklist 2</p>
+          </label>
+
+          <label htmlFor="pinned-tasklist-3" className="menu-content__radio">
+            <div className="menu-content__color-block color-block--default"></div>
+            <input
+              type="radio"
+              name="pinned-lists"
+              id="pinned-tasklist-3"
+              value="pinned-tasklist-3"
+              onChange={pinnedTasklistChangeHandler}
+              checked={pinnedTasklist === "pinned-tasklist-3"}
+            />
+            <p className="menu-content__p">Tasklist 3</p>
+          </label>
+
         </div>
       </div>
 
