032ad0279fc6ec795dc0b44e21c684c9006be034
Gr34sy <slupek3@poczta.fm>
2023-10-11 05:06:19 +0200

    added tasklist to pinned in homepage

---
+++ src/components/Tasklist/Tasklist.js
@@ -0,0 +1,21 @@
+
+export function  Tasklist(){
+    return (
+        <div className="tasklist">
+            <h3 className="list__title">List Title</h3>
+            <ul className="list__ul">
+                <li>
+                    Ziemniaki
+                </li>
+
+                <li>
+                    Szynka
+                </li>
+
+                <li>
+                    Syr
+                </li>
+            </ul>
+        </div>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import { FormLogIn } from "@/components/Forms/FormLogIn";
+
 import { List } from '../components/List/List';
+import { Tasklist } from "@/components/Tasklist/Tasklist";
 
 export default function HomePage() {
   return (
     <div className="homepage">
-      <main className="main">
+      <main className="main homepage__pinned">
         <h1 className="section__header">Pinned Lists</h1>
         <div className="homepage__pinned-lists">
           <List />
           <List />
           <List />
         </div>
-
-        <div className="homepage__pinned-tasklist">
-
-        </div>
+       
+        <Tasklist/>
       </main>
 
       <section className="section">
