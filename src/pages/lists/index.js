550e2e7bfd2af59c8181f6c5f6b59af378b78cb8
Gr34sy <slupek3@poczta.fm>
2023-10-12 03:39:09 +0200

    added subpages of listpage

--- src/pages/lists/index.js
+++ src/pages/lists/index.js
@@ -1,15 +1,45 @@
 import { FormCreateSimpleList } from "@/components/Forms/FormCreateSimpleList";
 import { FormCreateTaskList } from "@/components/Forms/FormCreateTaskList";
+import { ChoiceTile } from "@/components/ChoiceTile/ChoiceTile";
 
 function ListsPage() {
   return (
-    <section className="section">
-      <h1 className="section__header">Lists Page</h1>
-      <FormCreateSimpleList />
-      <br /><br /><br />
-      <FormCreateTaskList />
-    </section>
+    <main className="main lists-page">
+      <div className="browse-lists">
+        <h1 className="section__header">Browse Your Lists</h1>
+        <div className="browse-lists__choice-tiles">
+          <ChoiceTile
+            link={"/lists/simple-lists"}
+            content={"Your Simple Lists"}
+          />
+          <ChoiceTile
+            link={"/lists/tasklists"}
+            content={"Your Tasklists"}
+          />
+        </div>
+      </div>
+
+      <div className="create-list">
+        <h2 className="section__header">Create New List</h2>
+        <div className="create-list__choice-tiles">
+          <ChoiceTile
+            link={"/lists/simple-list-creator"}
+            content={"Simple List Creator"}
+          />
+          <ChoiceTile
+            link={"/lists/tasklist-creator"}
+            content={"Tasklist Creator"}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
 
 export default ListsPage;
+
+{
+  /* <FormCreateSimpleList />
+<br /><br /><br />
+<FormCreateTaskList /> */
+}
---
+++ src/pages/lists/tasklist-creator/index.js
@@ -0,0 +1,10 @@
+
+function ListCreatorPage(){
+    return (
+        <h1>
+            List Creator Page
+        </h1>
+    )
+}
+
+export default ListCreatorPage;
