f1c8c777d10a6b8b63f8894518859dfddb608f0e
Gr34sy <slupek3@poczta.fm>
2023-12-23 14:19:43 +0100

    added dummy-data file

---
+++ src/utils/dummy-data.js
@@ -0,0 +1,21 @@
+export const Dummy_Tasklist = {};
+
+export const Dummy_List = {};
+
+export const Dummy_User_Data = {
+    user_name: 'Dummy User',
+    user_type: 'Github',
+    user_options: {
+        theme: 'default',
+        pinned_task_list: 'TL1',
+        pinned_simple_list: 'SL1',
+    },
+    user_lists: {
+        simple_lists: [
+
+        ],
+        task_lists: [
+
+        ],
+    },
+};
