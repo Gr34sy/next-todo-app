54cbfd89c2d39dc4ccdb4bf6c8e2a6475a4f0e3a
Gr34sy <slupek3@poczta.fm>
2023-12-25 13:50:04 +0100

    added dummy list and tasklist data

--- src/utils/dummy-data.js
+++ src/utils/dummy-data.js
@@ -1,5 +1,3 @@
-export const Dummy_Tasklist = {};
-
 export const Dummy_List = {
     list_id: 'dummy-list1',
     title: 'Dummy List ',
@@ -8,27 +6,80 @@ export const Dummy_List = {
     tasks: [
         {
             task_id: 'dummy-task1',
-            task_name: 'This is dummy task 1',
+            task_title: 'This is dummy task 1',
             isDone: false,
         },
 
         {
             task_id: 'dummy-task2',
-            task_name: 'This is dummy task 2',
+            task_title: 'This is dummy task 2',
             isDone: false,
         },
 
         {
             task_id: 'dummy-task3',
-            task_name: 'This is dummy task 3',
+            task_title: 'This is dummy task 3',
             isDone: true,
         },
 
         {
             task_id: 'dummy-task4',
-            task_name: 'This is dummy task 4',
+            task_title: 'This is dummy task 4',
             isDone: false,
         },
+    ],
+};
+
+export const Dummy_Tasklist = {
+    list_id: 'dummy-tasklist1',
+    title: 'Dummy Task List ',
+    deadline: '22-02-2024',
+    tags: ['Dummy', 'Test', 'Task List'],
+    description: 'Dummy description.',
+    pinnded: false,
+    tasks: [
+        {
+            task_id: 'dummy-task1',
+            task_title: 'This is dummy task 1',
+            deadline: '01-01-2024',
+            description: 'This is dummy task 1 description',
+            isDone: false,
+            operations: [
+                {
+                    operation_id: 'dummy-operation1',
+                    operation_title: ' Dummy operation 1 of task 1',
+                    isDone: false,
+                },
+
+                {
+                    operation_id: 'dummy-operation2',
+                    operation_title: ' Dummy operation 2 of task 1',
+                    isDone: false,
+                },
+            ],
+        },
+
+        {
+            task_id: 'dummy-task2',
+            task_title: 'This is dummy task 2',
+            deadline: '01-01-2024',
+            description: 'This is dummy task 2 description',
+            isDone: false,
+            operations: [
+                {
+                    operation_id: 'dummy-operation1',
+                    operation_title: ' Dummy operation 1 of task 2',
+                    isDone: false,
+                },
+
+                {
+                    operation_id: 'dummy-operation2',
+                    operation_title: ' Dummy operation 2 of task 2',
+                    isDone: false,
+                },
+            ],
+        },
+
     ],
 };
 
