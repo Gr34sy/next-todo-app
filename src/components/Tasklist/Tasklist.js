a0799336d252473967ec8ccb76e378179323638d
Gr34sy <slupek3@poczta.fm>
2024-01-08 02:15:42 +0100

    added list layout to tasklist page

--- src/components/Tasklist/Tasklist.js
+++ src/components/Tasklist/Tasklist.js
@@ -1,21 +1,16 @@
+import Link from "next/link";
 
-export function  Tasklist(){
-    return (
-        <div className="tasklist">
-            <h3 className="list__title">List Title</h3>
-            <ul className="list__ul">
-                <li>
-                    Ziemniaki
-                </li>
+export function Tasklist() {
+  return (
+    <Link href="/lists/tasklist" className="tasklist">
+        <h3 className="list__title">List Title</h3>
+        <ul className="list__ul">
+          <li>Ziemniaki</li>
 
-                <li>
-                    Szynka
-                </li>
+          <li>Szynka</li>
 
-                <li>
-                    Syr
-                </li>
-            </ul>
-        </div>
-    );
+          <li>Syr</li>
+        </ul>
+    </Link>
+  );
 }
--- src/pages/lists/tasklist.js
+++ src/pages/lists/tasklist.js
@@ -1,5 +1,20 @@
-export default function Tasklist(){
-    return (
-        <h1>List</h1>
-    )
+import { ListLayout } from "@/components/ListLayout/ListLayout";
+import { useState } from "react";
+
+export default function Tasklist() {
+  const [enableEdit, setEnableEdit] = useState(false);
+
+  function handleClick() {
+    setEnableEdit((prev) => !prev);
+  }
+
+  return (
+    <ListLayout
+      title={"Tasklist"}
+      onClick={handleClick}
+      enableEdit={enableEdit}
+    >
+      <div className="task-list"></div>
+    </ListLayout>
+  );
 }
